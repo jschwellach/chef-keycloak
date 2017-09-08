1b9719200ed99e62ba4776b27223b19381e6f68a
rodel-talampas <rodel.talampas@helixleisure.com>
2017-09-08 18:39:41 +1000

    added chef_version

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache License, Version 2.0'
 description      'Installs/Configures keycloak'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.2.1'
+chef_version '>= 12.5' if respond_to?(:chef_version)
 
 supports 'centos', '>= 6.2'
 supports 'amazon', '>= 2016.09'
